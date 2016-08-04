9baccbbe5cc07f603746f395d9acf1e6d4698f9a
webgen <webgen@outlook.com>
2016-08-04 18:27:12 +0400

    ready to rock

--- app/controllers/professor/application_controller.rb
+++ app/controllers/professor/application_controller.rb
@@ -1,8 +1,5 @@
 class Professor::ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  render 'layouts/professor/application'
+  layout 'professor/application'
   before_action :authenticate_professor!
-  before_action do
-    redirect_to :new_professor_session_path unless current_professor
-  end
 end
